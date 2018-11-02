b3685efd70ec57fb2cf5e39df34b410ff4d72e9a
zhangjie <zhangjie@game-reign.com>
2018-11-02 15:58:29 +0800

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Lionel Zhang on 2018/11/2.
+//  Copyright © 2018 Lionel Zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
