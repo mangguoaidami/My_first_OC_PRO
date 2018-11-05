71df4b49f7f847fd63cb27ac1640b92b748bf8cf
zhangjie <zhangjie@game-reign.com>
2018-11-05 17:27:04 +0800

    First commit.

--- My First Project/main.m
+++ My First Project/main.m
@@ -9,9 +9,38 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+//        int num = 400;
+//        num = num + 100;
+//        NSLog(@"num equals %i", num);
+        
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
+        answer = arc4random() % 100 + 1;
+//        NSLog(@"The random value is %i", answer);
+//        NSLog(@"Enter a number between 1 and 100");
+//        scanf("%i", &guess);
+//        NSLog(@"You entered: %i", guess);
+        while (guess != answer) {
+            turn ++;
+            NSLog(@"Guess #%i: Enter a number between 1 and 100", turn);
+            scanf("%i", &guess);
+            
+            if(guess > answer){
+                NSLog(@"Lower!");
+            }
+            else if(guess < answer){
+                NSLog(@"Higher!");
+            }
+            else{
+                NSLog(@"Correct! The answer was %i", answer);
+            }
+        };
+        
+        NSLog(@"It took you %i tries", turn);
     }
     return 0;
 }
